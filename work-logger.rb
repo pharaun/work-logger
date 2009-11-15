997bdcfe1c30c98137eb446f90c26cd943118a58
Anja Berens <pharaun666@gmail.com>
2009-11-14 23:35:33 -0500

    Finished basic "text handling" functionality
    TODO:
        * Undo & Redo functionality
        * File menu functionality
        * Toolbar functionality
        * Help Menu functionality (really easy)
        * Cleaning up and re-organizing the code better

--- work-logger.rb
+++ work-logger.rb
@@ -25,14 +25,13 @@ class Work_logger
 	@open = builder.get_object('open')
 	@close = builder.get_object('close')
 	@quit = builder.get_object('quit')
-	
+
 	# Edit menu
 	@undo = builder.get_object('undo')
 	@redo = builder.get_object('redo')
 	@cut = builder.get_object('cut')
 	@copy = builder.get_object('copy')
 	@paste = builder.get_object('paste')
-	@delete = builder.get_object('delete')
 	@clear_all = builder.get_object('clear_all')
 	@select_all = builder.get_object('select_all')
 
@@ -45,11 +44,119 @@ class Work_logger
 	@today = builder.get_object('today')
 	@forward = builder.get_object('forward')
 	@save_entry = builder.get_object('save_entry')
-	
-	# Main view & Status Bar	
+
+	# Main view & Status Bar
 	@textview = builder.get_object('textview')
 	@statusbar = builder.get_object('statusbar')
-	
+
+	# Initalize the signals
+	textview_init
+	date_init
+	signal_init
+    end
+
+
+    def textview_init
+	text_buffer = Gtk::TextBuffer.new
+	@textview.buffer = text_buffer
+
+	##############################
+	# Text buffer/viewer signals
+	##############################
+    end
+
+    def date_init
+
+    end
+
+
+    def signal_init
+	##############################
+	# File menu
+	##############################
+	@new.signal_connect('activate') do
+	    puts "Create a new sqlite db"
+	end
+
+	@open.signal_connect('activate') do
+	    puts "Open the selected sqlite db"
+	end
+
+	@close.signal_connect('activate') do
+	    puts "Tidying up and close the currently open sqlite db"
+	end
+
+	@quit.signal_connect('activate') { quit }
+	@window.signal_connect('destroy') { quit }
+
+	##############################
+	# Edit menu
+	##############################
+	@undo.signal_connect('activate') do
+	    puts "Undo the previous action on the textview"
+	end
+
+	@redo.signal_connect('activate') do
+	    puts "Redo the previous action on the textview"
+	end
+
+	@cut.signal_connect('activate') do
+	    @textview.signal_emit('cut_clipboard')
+	end
+
+	@copy.signal_connect('activate') do
+	    @textview.signal_emit('copy_clipboard')
+	end
+
+	@paste.signal_connect('activate') do
+	    @textview.signal_emit('paste_clipboard')
+	end
+
+	@clear_all.signal_connect('activate') do
+	    (@textview.buffer).set_text("")
+	end
+
+	@select_all.signal_connect('activate') do
+	    buffer = @textview.buffer
+
+	    buffer.place_cursor(buffer.end_iter)
+	    buffer.move_mark(buffer.get_mark('selection_bound'), buffer.start_iter)
+	end
+
+	##############################
+	# Help menu
+	##############################
+	@about.signal_connect('activate') { about_dialog }
+
+	##############################
+	# Toolbar
+	##############################
+	@back.signal_connect('clicked') do
+	    puts "Go back one day on the textview"
+	end
+
+	@today.signal_connect('clicked') do
+	    puts "Go to today on the textview"
+	end
+
+	@forward.signal_connect('clicked') do
+	    puts "Go forward one day on the textview"
+	end
+
+	@save_entry.signal_connect('clicked') do
+	    puts "Save the current entry to the db"
+	end
+    end
+
+
+    def quit
+	puts "Tidying up and quitting the program"
+	Gtk.main_quit
+    end
+
+
+    def about_dialog
+	puts "The about dialog"
     end
 
 
@@ -57,6 +164,4 @@ class Work_logger
 	@window.show_all
 	Gtk.main
     end
-
-
 end
