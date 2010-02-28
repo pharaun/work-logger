1b1d1328319b118b33c599e2f516932624e253f6
Anja Berens <pharaun666@gmail.com>
2010-02-28 01:17:38 -0500

    added a bit stuff to the config, mainly loading

--- config.rb
+++ config.rb
@@ -6,10 +6,48 @@
 # the GPL's licence.
 #
 
+require 'yaml'
+
 module Work
     class Config
 	def initialize
 	    puts "config"
+
+	    # Locate a config file
+	    file = locate_config
+
+	    if file != nil
+		puts "Loading config..."
+		@config = YAML.load_file(file)
+	    end
+	end
+	
+	def save_config
+	end
+
+	private
+	def locate_config
+	    location = [
+	    './.workloggerrc',
+	    '~/.workloggerrc',
+	    '~/.worklogger/config',
+	    '/etc/worklogger/config'
+	    ]
+
+	    location.each do |loc|
+		file = File.expand_path(loc)
+		
+		if File.file?(file)
+		    if File.readable?(file)
+			return file
+			break
+		    else
+			raise "Can't read the config file: #{file}"
+		    end
+		end
+	    end
+
+	    return nil
 	end
     end
 end
